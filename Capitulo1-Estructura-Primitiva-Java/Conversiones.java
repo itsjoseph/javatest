23f294896539c9779b7311e597515c65c566c68c
conor <conor@localhost.localdomain>
2026-02-25 16:29:57 -0600

    Estructura

---
+++ Capitulo1-Estructura-Primitiva-Java/Conversiones.java
@@ -0,0 +1,9 @@
+public class Conversiones {
+    public static void main(String[] args){
+        double quotient;
+        int x = 3;
+        int y = 10;
+        quotient = (double)x / y;
+        System.out.println(quotient);
+    }
+}
