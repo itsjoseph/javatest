cf45ed2f9fdc51aedbf196ce5f3735813c629de5
conor <conor@localhost.localdomain>
2026-02-25 15:29:02 -0600

    Modificaciones

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
-    public static void main (String []args){
-        System.out.println("salida desde argumentos" + args);
+    public static void main (String[] args){
+        System.out.println("salida desde argumentos");
+
+        byte bytes = 127;
+        System.out.println("salida byte: " + bytes);
     }
 }
