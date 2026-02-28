cbe75f605efd4188a0ff4b5e4327ba13d7be3f12
conor <conor@localhost.localdomain>
2026-02-27 20:56:36 -0600

    cap 2

---
+++ Capitulo2-Tipos-de-referencia/asignaciones/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+        String salida = "Salida";
+        System.out.println(salida);
+        salida = "salida 2.0";
+        System.out.println(salida);
+        salida.concat("salidasss");
+        
+        System.out.println(salida.concat("salidasss"));
+        System.out.println(salida);
+    }
+
+}
